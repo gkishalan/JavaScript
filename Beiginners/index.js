4a851b3bb42a3322ef1862608321c9211f41961d
gkishalan <princekishalan655@gmail.com>
2025-09-18 19:10:29 +0530

    Arrays , Functions

--- Beiginners/index.js
+++ Beiginners/index.js
@@ -12,3 +12,53 @@ let age = 20;
 let isApproved = false;
 let fisrtName = undefined;
 let selectedColor = null;
+
+//Objects
+let company = {
+    location : 'Colombo',
+    rating : 4.5
+}
+
+//Changing the properties in an object
+company.location = 'Jaffna';
+console.log(company);
+
+company['rating'] = 4.2;
+console.log(company);
+
+//Arrays
+let selectedItems = ['Car','Bus'];
+console.log(selectedItems);
+
+console.log(selectedItems[0]);
+
+//Adding new items to array
+let fruits = ['Apple','Orange'];
+fruits[2] = 1;
+console.log(fruits);
+
+//Calculating lenthg of an array 
+console.log(fruits.length)
+
+
+//Functions
+function greet(){
+    console.log('Hello my Friend!');
+}
+
+greet();
+
+//Arguments & Parametters in Functions 
+function enter(userName){
+    console.log('Hello' +' ' + userName)
+}
+
+enter('Kishalan');
+enter('Ronaldo')
+
+//Setting multiple Arguments
+function detail(fName,lName){
+    console.log('Hello' +' ' + fName + ' ' + lName)
+}
+
+detail('Cristiano','Ronaldo')

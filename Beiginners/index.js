98df48423c78a5fa7854fb638a3bf8ae94435a03
gkishalan <princekishalan655@gmail.com>
2025-09-18 13:13:05 +0530

    first javascript

---
+++ Beiginners/index.js
@@ -0,0 +1,14 @@
+//Declaring Varibale
+let name = 'Kishalan';
+console.log(name);
+
+//Using Constant 
+const interstRate = 0.3;
+console.log(interstRate);
+
+//Primitive Types
+let user = 'Mosh';
+let age = 20;
+let isApproved = false;
+let fisrtName = undefined;
+let selectedColor = null;

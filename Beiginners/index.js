2cc7c699c03bdb69a193c69fa28faf73b7b1db34
gkishalan <princekishalan655@gmail.com>
2025-09-19 09:03:27 +0530

    Variable Exercise

--- Beiginners/index.js
+++ Beiginners/index.js
@@ -69,4 +69,4 @@ function square(number){
     return number*number;
 }
 
-console.log(square(2))
+console.log(square(2));
---
+++ Error_Makes_Clever/Variables.js
@@ -0,0 +1,16 @@
+//Exercise 1
+/*
+1. Create a Variable named "Price" and set it to a specific price value.
+2. Create a variable named "Product" and set it to a product name.
+3. Create a Variable named "Tax" and set it to a specific tax value.
+4. Print the name of the product.
+5. Calculate the total price amount , including tex (total = Price + Tax ), and then print it.
+*/
+
+var price = 100;
+var product = 'apple';
+var tax = 10/100 * price;
+var total = price + tax
+
+console.log('The Product name is ' + ' ' + product);
+console.log('The Total Price : ' + ' ' + total);
